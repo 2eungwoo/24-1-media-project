9015ea8ed7b7c9f8355a038b65fd571b64630f4f
swlee <swo98@ajou.ac.kr>
2024-03-18 21:40:25 +0900

    first setting

---
+++ src/main/java/mediaproject/its/repository/Test.java
@@ -0,0 +1,36 @@
+package mediaproject.its.repository;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void createDate(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
