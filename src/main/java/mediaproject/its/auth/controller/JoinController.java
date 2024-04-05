3383c96e3391cfe77290da84341948ad325430ed
swlee <swo98@ajou.ac.kr>
2024-04-05 15:40:38 +0900

    merge with develop2

--- src/main/java/mediaproject/its/auth/controller/JoinController.java
+++ src/main/java/mediaproject/its/auth/controller/JoinController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import mediaproject.its.response.dto.CommonResponseDto;
 import mediaproject.its.domain.dto.JoinDto;
 import mediaproject.its.auth.service.JoinService;
-import mediaproject.its.response.exception.DuplicateMemberException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
