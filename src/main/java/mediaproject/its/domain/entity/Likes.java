9744004fa1d6691e370a7ffdf88a04781f7966a1
swlee <swo98@ajou.ac.kr>
2024-04-26 15:51:16 +0900

    likes entity

---
+++ src/main/java/mediaproject/its/domain/entity/Likes.java
@@ -0,0 +1,35 @@
+package mediaproject.its.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name="likes_unique_key",
+                        columnNames = {"user_id","post_id"}
+                )
+        }
+)
+public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+}
