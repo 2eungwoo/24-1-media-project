8d630a21f40532855b6450b24a55d549f40ae909
swlee <swo98@ajou.ac.kr>
2024-05-18 17:08:17 +0900

    no partition version

--- src/main/java/mediaproject/its/domain/repository/PostContentRepository.java
+++
@@ -1,7 +0,0 @@
-//package mediaproject.its.domain.repository;
-//
-//import mediaproject.its.domain.entity.PostContent;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface PostContentRepository extends JpaRepository<PostContent,Integer> {
-//}
